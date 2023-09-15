289d624063460b63c314eae729cd5ba814bfec9b
P. L. Lim <2090236+pllim@users.noreply.github.com>
2023-09-14 21:36:31 -0400

    API: Remove deprecated timeout.
    TST: Clean up tox, matrix, test header, etc.
    
    MNT: Fix setup.cfg complains

--- conftest.py
+++ conftest.py
@@ -9,6 +9,7 @@ def pytest_configure(config):
     PYTEST_HEADER_MODULES.pop('Pandas')
     PYTEST_HEADER_MODULES['astropy'] = 'astropy'
     PYTEST_HEADER_MODULES['requests'] = 'requests'
+    PYTEST_HEADER_MODULES['crds'] = 'crds'
 
     from ci_watson.version import version
     TESTED_VERSIONS['ci-watson'] = version
